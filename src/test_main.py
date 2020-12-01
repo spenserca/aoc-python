e7cdb07d7135cf5701c61bf00d82161da4c7ba84
spenserca <spenserca@gmail.com>
2020-12-01 07:22:17 -0600

    add test for actual 2020 day one part one output

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,7 @@
 import _2018.day_one.solution
 import _2019.day_one.solution
 import _2019.day_two.solution
+import _2020.day_one.solution
 
 
 def test_2018_day_one_part_one_returns_correct_value():
@@ -26,3 +27,8 @@ def test_2019_day_two_part_one_returns_correct_value():
 def test_2019_day_two_part_two_returns_correct_value():
     actual = _2019.day_two.solution.day_two_part_two()
     assert actual == 3892
+
+
+def test_2020_day_one_part_one_returns_correct_value():
+    actual = _2020.day_one.solution.day_one_part_one()
+    assert actual == 1010884
