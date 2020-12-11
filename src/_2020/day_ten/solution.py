14e3c1bc6d893f2e251ca3f21e2896925d01feaa
Spenser Andreassen <spenserca@gmail.com>
2020-12-11 16:21:43 -0600

    2020 day ten part two tests passing - wrong actual answer

--- src/_2020/day_ten/solution.py
+++ src/_2020/day_ten/solution.py
@@ -1,7 +1,13 @@
-from _2020.day_ten.utils import get_product_of_one_and_three_jolt_differences
+from _2020.day_ten.utils import get_product_of_one_and_three_jolt_differences, \
+    get_count_of_distinct_adapter_configurations
 from _2020.utils.input_utils import get_input_lines
 
 
 def day_ten_part_one():
     input_lines = get_input_lines('day_ten')
     return get_product_of_one_and_three_jolt_differences(input_lines)
+
+
+def day_ten_part_two():
+    input_lines = get_input_lines('day_ten')
+    return get_count_of_distinct_adapter_configurations(input_lines)
--- src/_2020/day_ten/utils.py
+++ src/_2020/day_ten/utils.py
@@ -18,6 +18,36 @@ def get_product_of_one_and_three_jolt_differences(output_joltages: [str]):
 
     return prod(joltage_distribution)
 
-
+#  1962331 too low
 def get_count_of_distinct_adapter_configurations(output_joltages: [str]):
-    return -1
+    joltages = sorted([int(oj) for oj in output_joltages])
+    current_connector_joltage_rating = 0
+
+    adapter_configurations = dict()
+    i = 0
+    while i <= len(joltages):
+        valid_adapters = list(filter(lambda j: j - current_connector_joltage_rating <= 3, joltages[i:i + 3]))
+
+        if i != len(joltages):
+            adapter_configurations[current_connector_joltage_rating] = valid_adapters
+            current_connector_joltage_rating = valid_adapters[0]
+        else:
+            adapter_configurations[current_connector_joltage_rating] = [current_connector_joltage_rating + 3]
+
+        i += 1
+
+    return len(get_connected_adapters(adapter_configurations, 0))
+
+
+def get_connected_adapters(adapter_configurations: dict, source_adapter: int):
+    connected_adapters = []
+    for ca in adapter_configurations[source_adapter]:
+        output_str = str(source_adapter)
+        if ca in adapter_configurations.keys():
+            child_adapters = get_connected_adapters(adapter_configurations, ca)
+            for child in child_adapters:
+                connected_adapters.append(output_str + '|' + str(child))
+        else:
+            connected_adapters.append(output_str)
+
+    return connected_adapters
